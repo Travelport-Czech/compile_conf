2ad9ebd38e054cc1f8ddd392546675b5854d753f
Petr Coufal <petr.coufal@travelportgds.cz>
2020-09-03 10:42:21 +0200

    Add new stages robots, pipeline

---
+++ _tests/test2.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+result='Ok'
+
+echo "Test config is generated correctly for all stages:"
+for stage in production testing internal support devel robots pipeline; do
+   ../compile_conf "${stage}" test.stages.conf.sample test.stages.conf
+   read -r firstline < test.stages.conf
+    
+   if [[ "$stage" == "$firstline" ]]; then
+     echo "$stage OK" 
+   else
+     echo "$stage Failed" 
+     cat test.stages.conf
+     result='Failed'
+   fi
+   rm -f test.stages.conf
+done
+
+
+
+printf '\n\n====== Summary ======\n'
+if [[ "$result" == 'Ok' ]]; then
+  printf 'Everything is OK.\n\n'
+else
+  printf 'At least one test has failed!\n\n'
+  exit 1
+fi
